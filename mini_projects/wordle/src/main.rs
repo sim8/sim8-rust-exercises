66213c78cb048503bffb1572ce93a6c10b03e068
Simeon Lees <simeon.k.lees@gmail.com>
2022-02-15 23:05:26 +0000

    Rough code for guesses + results

--- mini_projects/wordle/src/main.rs
+++ mini_projects/wordle/src/main.rs
@@ -1,5 +1,17 @@
 use std::io;
 
+// TODO
+// - use constant for get_result length. usize?
+
+#[derive(Copy, Clone)]
+enum LetterResult {
+    Correct,
+    WrongLocation,
+    Wrong,
+}
+
+const WORD_LENGTH: usize = 5;
+
 fn main() {
     let word = "CANAL";
     println!("");
@@ -12,10 +24,29 @@ fn main() {
 
     let guess = enter_guess();
     println!("You guessed: {}", guess);
+
+    let result = get_result(guess, word);
+    print_result(result);
+    println!("");
 }
 
 fn render_next_guess_placeholder() {
     println!("[][][][][]")
+}
+
+fn get_result(guess: String, word: &str) -> [LetterResult; WORD_LENGTH] {
+    let result: [LetterResult; WORD_LENGTH] = [LetterResult::Correct; WORD_LENGTH];
+    return result;
+}
+
+fn print_result(result: [LetterResult; WORD_LENGTH]) {
+    for letter_result in result {
+        match letter_result {
+            LetterResult::Correct => print!("O"),
+            LetterResult::WrongLocation => print!("o"),
+            LetterResult::Wrong => print!("X"),
+        }
+    }
 }
 
 fn enter_guess() -> String {
